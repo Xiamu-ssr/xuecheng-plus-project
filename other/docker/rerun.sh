65fd993410440a220f20480b7e974552020f51f7
Xiamu-ssr <1138882663@qq.com>
2024-02-20 14:29:59 +0800

    update

--- other/docker/rerun.sh
+++ other/docker/rerun.sh
@@ -13,13 +13,19 @@ else
 fi
 
 # 将jar移动到宿主机
-mkdir /root/springcloud
+#docker-compose -f docker-compose.yml up -d
+#todo
+#替换为云服务器实际路径
+export MY_DIR=/root/xuecheng-plus-guide/docker
 
-cp -r ./xuecheng-plus-gateway/target/xuecheng-plus-gateway.jar													/root/springcloud/xuecheng-plus-gateway.jar
-cp -r ./xuecheng-plus-learning/xuecheng-plus-learning-api/target/xuecheng-plus-learning-api.jar 				/root/springcloud/xuecheng-plus-learning-api.jar
-cp -r ./xuecheng-plus-orders/xuecheng-plus-orders-api/target/xuecheng-plus-orders-api.jar 						/root/springcloud/xuecheng-plus-orders-api.jar
-cp -r ./xuecheng-plus-content/xuecheng-plus-content-api/target/xuecheng-plus-content-api.jar 					/root/springcloud/xuecheng-plus-content-api.jar
-cp -r ./xuecheng-plus-media/xuecheng-plus-media-api/target/xuecheng-plus-media-api.jar							/root/springcloud/xuecheng-plus-media-api.jar
-cp -r ./xuecheng-plus-auth/target/xuecheng-plus-auth.jar														/root/springcloud/xuecheng-plus-auth.jar
-cp -r ./xuecheng-plus-search/target/xuecheng-plus-search.jar													/root/springcloud/xuecheng-plus-search.jar
-cp -r ./xuecheng-plus-system/xuecheng-plus-system-api/target/xuecheng-plus-system-api.jar						/root/springcloud/xuecheng-plus-system-api.jar
+#
+cp -r ./xuecheng-plus-gateway/target/xuecheng-plus-gateway.jar													$MY_DIR/springcloud/xuecheng-plus-gateway.jar
+cp -r ./xuecheng-plus-learning/xuecheng-plus-learning-api/target/xuecheng-plus-learning-api.jar 				$MY_DIR/springcloud/xuecheng-plus-learning-api.jar
+cp -r ./xuecheng-plus-orders/xuecheng-plus-orders-api/target/xuecheng-plus-orders-api.jar 						$MY_DIR/springcloud/xuecheng-plus-orders-api.jar
+cp -r ./xuecheng-plus-content/xuecheng-plus-content-api/target/xuecheng-plus-content-api.jar 					$MY_DIR/springcloud/xuecheng-plus-content-api.jar
+cp -r ./xuecheng-plus-media/xuecheng-plus-media-api/target/xuecheng-plus-media-api.jar							$MY_DIR/springcloud/xuecheng-plus-media-api.jar
+cp -r ./xuecheng-plus-auth/target/xuecheng-plus-auth.jar														$MY_DIR/springcloud/xuecheng-plus-auth.jar
+cp -r ./xuecheng-plus-search/target/xuecheng-plus-search.jar													$MY_DIR/springcloud/xuecheng-plus-search.jar
+cp -r ./xuecheng-plus-system/xuecheng-plus-system-api/target/xuecheng-plus-system-api.jar						$MY_DIR/springcloud/xuecheng-plus-system-api.jar
+
+docker-compose -f $MY_DIR/docker-compose.yml up -d
